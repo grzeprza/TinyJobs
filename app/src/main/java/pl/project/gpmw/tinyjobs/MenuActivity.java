d961c77002476a7a4a3b0707d46a8f48a65fd727
grzeprza <gprzadka@interia.pl>
2016-03-09 16:08:25 +0100

    Commercial bug fixed. Finished FindTinyJobsActivity. Base to PlaceTinyJob

--- app/src/main/java/pl/project/gpmw/tinyjobs/MenuActivity.java
+++ app/src/main/java/pl/project/gpmw/tinyjobs/MenuActivity.java
@@ -1,9 +1,12 @@
 package pl.project.gpmw.tinyjobs;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
@@ -14,6 +17,7 @@ public class MenuActivity extends AppCompatActivity {
     private static final String TOAST_TEXT = "Test ads are being shown. "
             + "To show live ads, replace the ad unit ID in res/values/strings.xml with your own ad unit ID.";
 
+    Button button_findTinyJobs, button_placeTinyJobs, button_myTasks, button_getMorePoints;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +25,34 @@ public class MenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_menu);
 
         // Load an ad into the AdMob banner view.
-        AdView adView = (AdView) findViewById(R.id.adView);
+        AdView adView = (AdView) findViewById(R.id.menu_adView);
+     //   adView.setAdSize(com.google.android.gms.ads.AdSize.SMART_BANNER);
         AdRequest adRequest = new AdRequest.Builder()
                 .setRequestAgent("android_studio:ad_template").build();
         adView.loadAd(adRequest);
 
         // Toasts the test ad message on the screen. Remove this after defining your own ad unit ID.
         Toast.makeText(this, TOAST_TEXT, Toast.LENGTH_LONG).show();
+
+        //STARTS FIND TINY JOBS ACTIVITY
+        button_findTinyJobs = (Button) findViewById(R.id.button_findTinyJob);
+        button_findTinyJobs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(), FindTinyJobsActivity.class);
+                startActivity(i);
+            }
+        });
+
+        //STARTS PLACE TINY JOB ACTIVITY
+        button_placeTinyJobs = (Button) findViewById(R.id.button_placeTinyJob);
+        button_placeTinyJobs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent (v.getContext(), PlaceTinyJob.class);
+                startActivity(i);
+            }
+        });
     }
 
 
---
+++ app/src/main/java/pl/project/gpmw/tinyjobs/PlaceTinyJob.java
@@ -0,0 +1,13 @@
+package pl.project.gpmw.tinyjobs;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class PlaceTinyJob extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_place_tiny_job);
+    }
+}
